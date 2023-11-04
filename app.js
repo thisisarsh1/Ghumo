fc39b7df7a075b2917411081dff3d02348298014
Rehbar khan <rehbarkhan.2711@gmail.com>
2023-11-04 15:46:15 +0530

    basic functional

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const Listing = require ('/Users/rehbarkhan/Documents/codebox/major project/mode
 const path = require("path");
 const methodOverride = require ("method-override")
 const ejsMate = require("ejs-mate")
+
 main().then(()=>{
     console.log("Main is working!")
     
@@ -22,8 +23,13 @@ app.set("views",path.join(__dirname,"views"))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride("_method"))
 app.engine("ejs",ejsMate)
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
+//home
+app.get("/",async(req,res)=>{
+   
+res.render("listings/home.ejs")
+})
 
 //index Route!
 app.get("/listings",async(req,res)=>{
--- models/listing.js
+++ models/listing.js
@@ -1,40 +1,44 @@
-const mongoose =require ("mongoose");
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
-// const listingSchema= new Schema({
-//     title :{type :String,
-//         required:true
-//     },
-//     description:String,
-//     image: {
-//         default:"https://kubalubra.is/wp-content/uploads/2017/11/default-thumbnail.jpg",
-//     filename: String,
-//     url: String,
-//     set:(v)=>v===""? "https://kubalubra.is/wp-content/uploads/2017/11/default-thumbnail.jpg":v
-//     },
-//     price: Number,
-//     location:String,
-//     country:String,
-// })
-
-
-
-
-
-
 const listingSchema = new Schema({
-    title :{type :String,
-           },
-  description: String,
-  image: {
-   filename: String,
-    url: String,
-   }, 
-  price: Number,
-  location: String,
-  country: String,
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+    image: {
+        
+     
+            type: String,
+            default: "https://kubalubra.is/wp-content/uploads/2017/11/default-thumbnail.jpg",
+            set: function (v) {
+                return v === "" ? "https://kubalubra.is/wp-content/uploads/2017/11/default-thumbnail.jpg" : v;
+            
+            }
+    },
+    price: Number,
+    location: String,
+    country: String,
 });
+
+
+
+
+
+// const listingSchema = new Schema({
+//     title :{type :String,
+//            },
+//   description: String,
+//   image: {
+//    filename: String,
+//     url: String,
+    
+//    }, 
+//   price: Number,
+//   location: String,
+//   country: String,
+// });
 
 const Listing = mongoose.model('Listing', listingSchema);
 
